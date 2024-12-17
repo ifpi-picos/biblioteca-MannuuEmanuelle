7a2d1f666f096f1110914b644adae32603bf3b59
Emanuelle <capic.2024118tads0024@aluno.ifpi.edu.br>
2024-12-17 15:27:27 -0300

    Atualizando menu de opções

--- src/App.java
+++ src/App.java
@@ -6,32 +6,33 @@ public class App {
 
         Biblioteca biblioteca = new Biblioteca();
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("--- BIBLIOTECA ---");
-
-        OUTER:
-        while (true) {
-            System.out.println("1 - Cadastrar Livro ");
-            System.out.println("2 - Devolver livro ");
-            System.out.println("3 - Listar todos os livros ");
-            System.out.println("4 - Listar livros emprestados ");
-            System.out.println("5 - Listar livros disponíveis ");
-            System.out.println("6 - Listar histórico do usuário ");
-            System.out.println("7 - Pegar livro emprestado ");
-            System.out.println("8 - Sair ");
-            int opcao = scanner.nextInt();
-            scanner.nextLine();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("--- BIBLIOTECA ---");
             
-            switch (opcao) {
-                case 1 -> biblioteca.adicionarLivro();
-                default -> {
-                    break OUTER;
+            OUTER:
+            while (true) {
+                System.out.println("1 - Cadastrar Livro ");
+                System.out.println("2 - Devolver livro ");
+                System.out.println("3 - Listar todos os livros ");
+                System.out.println("4 - Listar livros emprestados ");
+                System.out.println("5 - Listar livros disponíveis ");
+                System.out.println("6 - Listar histórico do usuário ");
+                System.out.println("7 - Pegar livro emprestado ");
+                System.out.println("8 - Sair ");
+                int opcao = scanner.nextInt();
+                scanner.nextLine();
+                
+                switch (opcao) {
+                    case 1 -> biblioteca.adicionarLivro();
+                    case 3 -> biblioteca.listarLivros();
+                    case 4 -> biblioteca.listarLivrosDisponiveis();
+                    case 5 -> biblioteca.listarLivrosEmprestados();
+                    default -> {
+                        break OUTER;
+                    }
                 }
             }
         }
-
-        scanner.close();
 
 
 
